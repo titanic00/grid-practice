436b31768dcdd26699787b0678c97d338ef1aca0
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-05-02 20:22:07 +0300

    added burger menu for mobiles

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('.burger__list').click(function (event) {
+        $('.burger__list,.navbar,.wrapper,.header,.header__logo,.navbar__list,.main__login,.navbar__mobile').toggleClass('active');
+    })
+})
